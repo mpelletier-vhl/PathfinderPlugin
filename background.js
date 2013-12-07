6fb7e0344eef506c7cac2658dc5ff336e59b2ec5
Michael Pelletier <admin@mpelletier.net>
2013-12-06 22:19:42 -0500

    Add default settings for new users.

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   var my_settings = localStorage["mode"];
 
+  // Default setting for first time users.
+  if (my_settings === undefined) {
+  	my_settings = "gm";
+  }
+
   if (request.retrieve == "settings"){
     sendResponse({settings: my_settings});
   }
